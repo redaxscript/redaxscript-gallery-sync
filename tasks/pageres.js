c09ef31a87f20734eb7080201f4f2eed246ba588
redaxmedia <info@redaxmedia.com>
2019-02-16 21:26:57 +0100

    Add more urls

--- tasks/pageres.js
+++ tasks/pageres.js
@@ -21,7 +21,7 @@ module.exports = () =>
 			{
 				sizes:
 				[
-					'320x480'
+					'480x640'
 				],
 				dest: 'build/mobile'
 			}
@@ -31,7 +31,18 @@ module.exports = () =>
 			urls:
 			[
 				'https://demo.redaxscript.com',
-				'https://demo.redaxscript.com/login'
+				'https://demo.redaxscript.com?p=search/vero',
+				'https://demo.redaxscript.com?p=login',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/view/articles',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/edit/articles/1',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/new/articles',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/view/users',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/edit/users/1',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/new/users',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/view/groups',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/edit/groups/2',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/new/groups',
+				'https://demo.redaxscript.com?p=module/demo/login&redirect=admin/edit/settings'
 			],
 			format: 'jpg',
 			delay: 1,
