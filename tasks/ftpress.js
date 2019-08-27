532a81dd58f1c61cca6983692decb9e8f27dac92
redaxmedia <info@redaxmedia.com>
2019-08-27 18:58:51 +0200

    Auto confirm for SFTP

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -16,6 +16,7 @@ module.exports = () =>
 				url: process.env.FTP_URL,
 				command:
 				[
+					'set sftp:auto-confirm yes',
 					'mirror {SOURCE} {TARGET}-new-{TIMESTAMP} --reverse --delete-first --parallel=10 --use-pget-n=10',
 					'mv {TARGET} {TARGET}-old-{TIMESTAMP}',
 					'mv {TARGET}-new-{TIMESTAMP} {TARGET}',
